9e7bad1a9d523e74f37b877ea75a1ffeac9e84c3
atoder <alexandert@gmail.com>
2014-10-22 16:23:02 -0700

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | AFlexTweet"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | AFlexTweet"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | AFlexTweet"
+  end
 end
